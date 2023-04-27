1eff9366a3e1112e847b34a947c22b650fe2ee9d
Ranjan Manandhar <ranjan@ranjan.local>
2023-04-27 20:19:06 +0545

    add all

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form class="otc" name="one-time-code" action="#">
+        <fieldset>
+
+
+          <h2>Validation Code</h2>
+          {/* <label for="otc-1">Number 1</label>
+          <label for="otc-2">Number 2</label>
+          <label for="otc-3">Number 3</label>
+          <label for="otc-4">Number 4</label>
+          <label for="otc-5">Number 5</label>
+          <label for="otc-6">Number 6</label> */}
+          <div>
+            <input type="number" pattern="[0-9]*" value="" inputtype="numeric" autocomplete="one-time-code" id="otc-1" required />
+            <input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1" value="" inputtype="numeric" id="otc-2" required />
+            <input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1" value="" inputtype="numeric" id="otc-3" required />
+            <input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1" value="" inputtype="numeric" id="otc-4" required />
+            <input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1" value="" inputtype="numeric" id="otc-5" required />
+            <input type="number" pattern="[0-9]*" min="0" max="9" maxlength="1" value="" inputtype="numeric" id="otc-6" required />
+          </div>
+
+
+        </fieldset>
+      </form>
+
+
+
     </div>
   );
 }
